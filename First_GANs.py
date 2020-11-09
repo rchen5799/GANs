6e615d6fed4a2c667d6269c8035092e4c90c1b9a
rchen5799 <rchen5799@Gmail.com>
2020-11-08 23:18:48 -0500

    Continuation of Generator

--- First_GANs.py
+++ First_GANs.py
@@ -40,7 +40,18 @@ class GAN():
 
     def build_generator(self):
         model = Sequential()
-        model.add(Dense(256, input_dim=self.latent_dim)
+
+        model.add(Dense(256, input_dim=self.latent_dim))
         model.add(BatchNormalization(momentum = 0.8))
         model.add(Dense(512))
         model.add(LeakyReLU(alpha = 0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(Reshape(self.img_shape))
+
+        model.summary()
+
+        
