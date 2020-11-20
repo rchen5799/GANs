d6f58dcba2e4b88c003b87ef4866269eab8e0ac3
rchen5799 <rchen5799@Gmail.com>
2020-11-19 22:22:02 -0500

    sample

--- First_GANs.py
+++ First_GANs.py
@@ -96,4 +96,14 @@ class GAN():
         noise = np.random.normal(0, 1, (r * c, self.latent_dim))
         gen_imgs = self.generator.predict(noise)
         gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("images/%d.png" % epoch)
+        plt.close()
         
